9865d8d0f4d0eb15a1d3ef9fa824fd6b9cdd1bd1
Armbarbarian <daniel_goodall@outlook.com>
2020-06-03 15:02:56 +0100

    Changed the issue with the xticks to show only beginning of each month and renamed them 1st Feb etc..
    Using scale_x_discrete(breaks=c(dates), labels = c(labels)
    
    saved to PNG (attached)

--- cases_deaths_overlap_plots(mac).R
+++ cases_deaths_overlap_plots(mac).R
@@ -54,13 +54,35 @@ ggplot(UK) +
   theme(plot.title = element_text(size = 30, face = "bold"))
 
 # overlap of new cases and new deaths UK
+# scale_x_.... SO USEFUL to show the bins you want
+# https://rstudio-pubs-static.s3.amazonaws.com/3364_d1a578f521174152b46b19d0c83cbe7e.html
+# discrete _ limit = only shows those bins (3 dates)
+# discrete _ breaks = is what I wanted!! only show the xticks I want
 ggplot(UK) +
   geom_bar(aes(x = date, y = new_cases), fill = 'lightblue', stat='identity') +
   geom_line(aes(x = date, y = new_deaths), group=1, color='darkred', size = 1) +
   ggtitle('Overlap of New Deaths and Total Cases June 2nd') +
   theme(plot.title = element_text(size = 30, face = "bold")) +
-  xlab('Date') + ylab('')
+  xlab('Date') + 
+  ylab('') +
+  scale_x_discrete(breaks = c('2020-01-31', '2020-04-02', '2020-06-02'))
   
+
+# SAME AS ABOVE 
+# more custom
+# breaks of the beginning of the month
+# change the x labels to 1 Feb, 1 March, 1 April, 1 May, 1 June
+ggplot(UK) +
+  geom_bar(aes(x = date, y = new_cases), fill = 'lightblue', stat='identity') +
+  geom_line(aes(x = date, y = new_deaths), group=1, color='darkred', size = 1) +
+  ggtitle('Overlap of New Deaths and Total Cases June 2nd') +
+  theme(plot.title = element_text(size = 15, face = "bold")) +
+  xlab('Date') + 
+  ylab('') +
+  scale_x_discrete(breaks = c('2020-02-01', '2020-03-01', '2020-04-01', '2020-05-01', '2020-06-01'),
+                   labels = c('1st Feb','1st March','1st April','1st May','1st June')) 
+
+# ggsave('Overlap of New Deaths and Cases in the UK up to June 2nd.png')  
 
 
 
