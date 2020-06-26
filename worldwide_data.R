16bab030b2e82701c9eca445e57f365e10e62e57
Armbarbarian <daniel_goodall@outlook.com>
2020-06-26 17:37:38 +0100

    todays

---
+++ worldwide_data.R
@@ -0,0 +1,28 @@
+# total covid deaths and cases worldwide
+
+library('readxl')
+
+data <- read_excel('RAW_22june.xlsx')
+
+df <- data.frame(data)
+
+# change column name to country
+colnames(df)[7] <- 'country'
+colnames(df)[1] <- 'date'
+
+# df of todays numbers using dplyr
+library(dplyr)
+
+today <- filter(df, month==6, day==22)
+head(today)
+
+# add up the total cases and total deaths for ALL countries
+# for loop? NO NEED use sum() function
+
+cases <- sum(today$cases)
+deaths <- sum(today$deaths)
+cases  
+deaths
+
+# need to play all these together
+# for loop to add all the days into a new list then plot from that?
